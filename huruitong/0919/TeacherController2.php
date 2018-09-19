dbbcc78836f2b12d1f8c683f170833c8059bc1b3
huruitong <taoqimaohuruitong@163.com>
2018-09-19 20:04:21 +0800

    text

---
+++ huruitong/0919/TeacherController2.php
@@ -0,0 +1,15 @@
+<?php
+namespace app\index\controller;
+use app\common\model\Teacher;
+
+class TeacherController
+{
+    public function index()
+    {
+        $JiaoShiBiao = new Teacher;
+        $SuoYouJiaoShi = $JiaoShiBiao->select();
+        $JiaoShiZhangSan = $SuoYouJiaoShi[0];
+        echo "教师姓名".$JiaoShiZhangSan->getData('name').'<br />';
+        return '重复一遍：教师姓名'.$JiaoShiZhangSan->getData('name');
+    }
+}
