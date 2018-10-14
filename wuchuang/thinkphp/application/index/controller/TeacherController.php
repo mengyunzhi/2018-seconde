ca7723404268f50e11cfe9d5b0e8ad5a39d58a22
wuchuang123 <1076596804@qq.com>
2018-10-14 20:01:51 +0800

    学习内容 close 7

--- wuchuang/thinkphp/application/index/controller/TeacherController.php
+++ wuchuang/thinkphp/application/index/controller/TeacherController.php
@@ -43,13 +43,13 @@ class TeacherController extends Controller
         $Teacher->email = $postData['email'];
 
         //新增对象至数据表
-        $result = $Teacher->validate(ture)->save($Teacher->getData());
+        $result = $Teacher->validate(true)->save();
 
         //反馈结果
         if(false === $result)
         {
             return '新增失败:' . $Teacher->getError();
-        } else{
+        } else {
             return '新增成功。新增ID为:' . $Teacher->id;
         }
     }
@@ -57,5 +57,39 @@ class TeacherController extends Controller
     {
         $htmls = $this->fetch();
         return $htmls;
+    }
+    public function test()
+    {
+        $data = array();
+        $data['username'] = 'ce';
+        $data['name'] = '1';
+        $data['sex'] = '1';
+        $data['email'] = 'hello@hello.com';
+        var_dump($this->validate($data, 'Teacher'));
+    }
+    public function delete()
+    {
+        //获取pathinfo传入的ID值.
+        $id = Request::instance()->param('id/d'); //"/d表示将数转化为整型"
+
+        if (is_null($id) || 0 === $id) {
+            return $this->error('未获取到ID信息');
+        }
+
+        //获取要删除对象
+        $Teacher = Teacher::get($id);
+
+        //删除对象不存在
+        if(is_null($Teacher)) {
+            return $this->error('不存在id为' . $id . '的教师，删除失败');
+            }
+            
+        //删除对象
+        if (!$Teacher->delete()) {
+            return $this->error('删除失败:' . $Teacher->getError());
+        }
+        
+        //进行跳转
+        return $this->success('删除成功', url('index'));
     }
 }
