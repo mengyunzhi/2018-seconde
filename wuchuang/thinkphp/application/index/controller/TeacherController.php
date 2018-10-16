5342264f8d116f634559d9f14a860493d28ccfe0
wuchuang123 <1076596804@qq.com>
2018-10-16 22:19:51 +0800

    数据更新

--- wuchuang/thinkphp/application/index/controller/TeacherController.php
+++ wuchuang/thinkphp/application/index/controller/TeacherController.php
@@ -92,4 +92,42 @@ class TeacherController extends Controller
         //进行跳转
         return $this->success('删除成功', url('index'));
     }
+    public function edit()
+    {
+        // 获取传入ID
+        $id = Request::instance()->param('id/d');
+
+        // 在Teacher表模型中获取当前记录
+        if (is_null($Teacher = Teacher::get($id))) {
+            return '系统未找到ID为' . $id . '的记录';
+        }
+
+        // 将数据传给V层
+        $this->assign('Teacher', $Teacher);
+
+        // 获取封装好的V层内容
+        $htmls = $this->fetch();
+
+        // 将封装好的V层内容返回给用户
+        return $htmls;
+    }
+    public function update() 
+    {
+        // 接受数据，获取要更新的关键字信息
+        $id = Request::instance()->post('id/d');
+
+        // 获取当前对象
+        $Teacher = Teacher::get($id);
+
+        // 写入要更新的数据
+        $Teacher->name = Request::instance()->post('name');
+        $Teacher->username = Request::instance()->post('username');
+        $Teacher->sex = Request::instance()->post('sex/d');
+        $Teacher->email = Request::instance()->post('email');
+
+        // 更新
+        var_dump($Teacher->validate(true)->save());
+
+        return '更新成功';
+    }
 }
