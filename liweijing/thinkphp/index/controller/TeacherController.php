46f8e27ca5cd4b6f4cd96a3f3522f6834d875ba4
liweijing666 <liweijing523@163.com>
2018-11-01 22:09:59 +0800

    11.1

--- liweijing/thinkphp/index/controller/TeacherController.php
+++ liweijing/thinkphp/index/controller/TeacherController.php
@@ -7,80 +7,79 @@ use app\common\model\Teacher;  // 教师模型
 /**
  * 教师管理
  */
-class TeacherController extends Controller
-{
-    public function index()
-    {
+class TeacherController extends Controller {
+    public function index() {
         try {
             // 获取查询信息
             $name = Request::instance()->get('name');
             echo $name;
-        $pageSize = 5; // 每页显示5条数据
-        // 实例化Teacher
-    	$Teacher = new Teacher();
-        // 定制查询信息
-        if (!empty($name)) {
-            $Teacher->where('name','like','%'.$name.'%');
-        }
-        // 按条件查询数据并调用分页
-        $teachers = $Teacher->paginate($pageSize, false,
-            [
-                'query'=>['name' => $name,]
-            ]);
-    	// 向V层传递数据
-    	$this->assign('teachers', $teachers);
-    	// 取回打包后的数据
-    	$htmls = $this->fetch();
-    	// 将数据返回给用户
-    	return $htmls;
+            $pageSize = 5; // 每页显示5条数据
+            // 实例化Teacher
+            $Teacher = new Teacher();
+            // 定制查询信息
+            if (!empty($name)) {
+                $Teacher->where('name','like','%'.$name.'%');
+            }
+            // 按条件查询数据并调用分页
+            $teachers = $Teacher->paginate($pageSize, false, [
+                    'query'  => [
+                        'name' => $name
+                    ]
+                ]);
+            // 向V层传递数据
+            $this->assign('teachers', $teachers);
+            // 取回打包后的数据
+            $htmls = $this->fetch();
+            // 将数据返回给用户
+            return $htmls;
 
-        // 获取到ThinkPHP的内置异常时，直接向上抛出，交给ThinkPHP处理
-    } catch (\think\Exception\HttpResponseException $e) {
-        throw $e;
-        // 获取到正常的异常时，输出异常
-    } catch(\Exception $e) {
-        return $e->getMessage();
-    }
+            // 获取到ThinkPHP的内置异常时，直接向上抛出，交给ThinkPHP处理
+        } catch (\think\Exception\HttpResponseException $e) {
+            throw $e;
+            // 获取到正常的异常时，输出异常
+        } catch(\Exception $e) {
+            return $e->getMessage();
+        }
     }
     public function insert()
     {
-    	$message = '';  // 提示信息
+        $message = '';  // 提示信息
 
-        try{
+        try {
             // 接收传入数据
-    	$postData = Request::instance()->post();
+            $postData = Request::instance()->post();
 
-		//实例化Teacher空对象
-		$Teacher = new Teacher();
-		//为对象属性赋值
-		$Teacher->name = $postData['name'];
-		$Teacher->username = $postData['username'];
-		$Teacher->sex = $postData['sex'];
-		$Teacher->email = $postData['email'];
-		// 新增对象至数据表
-		$result = $Teacher->validate(true)->save($Teacher->getData());
-		// 反馈结果
-		if (false === $result) {
+            // 实例化Teacher空对象
+            $Teacher = new Teacher();
+            // 为对象属性赋值
+            $Teacher->name = $postData['name'];
+            $Teacher->username = $postData['username'];
+            $Teacher->sex = $postData['sex'];
+            $Teacher->email = $postData['email'];
+            // 新增对象至数据表
+            $result = $Teacher->validate(true)->save($Teacher->getData());
+            // 反馈结果
+            if (false === $result) {
             // 验证未通过，发生错误
-			$message = '新增失败：' . $Teacher->getError();
-		} else {
-            // 提示操作成功，并跳转至教师管理列表
-            return $this->success('用户' . $Teacher->name . '新增成功。',url('index'));
-		}
-    } catch(\Exception $e) {
-        // 发生异常
-        return $e->getMessage();
-    }
-    return $this->error($message);
+                $message = '新增失败：' . $Teacher->getError();
+            } else {
+                // 提示操作成功，并跳转至教师管理列表
+                return $this->success('用户' . $Teacher->name . '新增成功。',url('index'));
+            }
+        } catch(\Exception $e) {
+            // 发生异常
+            return $e->getMessage();
+        }
+        return $this->error($message);
     }
     public function add()
     {
-    	$htmls = $this->fetch();
-    	return $htmls;
+        $htmls = $this->fetch();
+        return $htmls;
     }
     public function delete()
     {
-    	try {
+        try {
             // 获取get数据
             $Request = Request::instance();
             $id = Request::instance()->param('id/d');
@@ -109,29 +108,29 @@ class TeacherController extends Controller
             return $e->getMessage();
         }
 
-        return $this->error($message);	
+        return $this->error($message);    
     }
     public function edit()
     {
-    	try{
-        // 获取传入ID
-    	$id = Request::instance()->param('id/d');
-    	// 判断是否成功接收
-        if (is_null($id) || 0 === $id) {
-            throw new \Exception('未获取到ID信息',1);
-        }
-        // 在Teacher表模型中获取当前记录
-        if (null === $Teacher = Teacher::get($id)) {
-            // 由于在$this->error抛出了异常，所以也可以省略return（不推荐）
-            $this->error('未找到ID为' . $id . '的记录');
-        }
-    	//将数据传给V层内容
-    	$this->assign('Teacher',$Teacher);
-    	//获取封装好的数据
-    	$htmls = $this->fetch();
-    	//将封装好的V层内容返回给用户
-    	return $htmls;
-        // 获取到ThinkPHP的内置异常时，直接向上抛出，交给ThinkPHP处理
+        try{
+            // 获取传入ID
+            $id = Request::instance()->param('id/d');
+            // 判断是否成功接收
+            if (is_null($id) || 0 === $id) {
+                throw new \Exception('未获取到ID信息',1);
+            }
+            // 在Teacher表模型中获取当前记录
+            if (null === $Teacher = Teacher::get($id)) {
+                // 由于在$this->error抛出了异常，所以也可以省略return（不推荐）
+                $this->error('未找到ID为' . $id . '的记录');
+            }
+            // 将数据传给V层内容
+            $this->assign('Teacher',$Teacher);
+            // 获取封装好的数据
+            $htmls = $this->fetch();
+            //将封装好的V层内容返回给用户
+            return $htmls;
+            // 获取到ThinkPHP的内置异常时，直接向上抛出，交给ThinkPHP处理
         } catch (\think\Exception\HttpResponseException $e) {
             throw $e;
             // 获取到正常的异常时，输出异常
