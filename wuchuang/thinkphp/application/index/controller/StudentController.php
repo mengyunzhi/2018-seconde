1fe9ad4cf1bb5be58f0d8d58ae938fcfb4bc9e0d
wuchuang123 <1076596804@qq.com>
2018-11-12 21:54:19 +0800

    学习内容

--- wuchuang/thinkphp/application/index/controller/StudentController.php
+++ wuchuang/thinkphp/application/index/controller/StudentController.php
@@ -1,5 +1,7 @@
 <?php
 namespace app\index\controller;
+use app\common\model\Klass;
+use think\Request;
 use app\common\model\Student;
 
 class StudentController extends IndexController
@@ -9,5 +11,22 @@ class StudentController extends IndexController
         $students = Student::paginate();
         $this->assign('students', $students);
         return $this->fetch(); 
+    }
+
+    public function edit()
+    {
+        $id = Request::instance()->param('id/d');
+
+        // 判断是否存在当前记录
+        if (is_null($Student = Student::get($id))) {
+            return $this->error('未找到ID为' . $id . '的记录');
+        }
+
+        // 取出班级列表
+        $klasses = Klass::all();
+        $this->assign('klasses', $klasses);
+
+        $this->assign('Student', $Student);
+        return $this->fetch();
     }
 }
