226a921d3b0625f4d2b8a3d358d9e37f6fb82d22
donghaotian <you@example.com>
2018-11-13 22:04:49 +0800

    thinkPHP5今日学习内容

--- donghaotian/thinkPHP/application/index/controller/TeacherController.php
+++ donghaotian/thinkPHP/application/index/controller/TeacherController.php
@@ -14,8 +14,31 @@ class TeacherController extends Controller
     public function index()
     {
         try {
+            // 获取查询信息
+            $name = Request::instance()->get('name');
+            echo $name;
+
+            $pageSize = 5; // 每页显示5条数据
+
+            // 实例化Teacher
             $Teacher = new Teacher; 
-            $teachers = $Teacher->select();
+            
+            // 实例化Teacher
+            $Teacher = new Teacher;
+
+            // 定制查询信息
+            if (!empty($name)) {
+                $Teacher->where('name', 'like', '%' . $name . '%');
+            }
+
+            // 按条件查询数据并调用分页
+            $teachers = $Teacher->paginate($pageSize, false, [
+                'query'=>[
+                    'name' => $name,
+                ],
+            ]);
+            // 调用分页
+            $teachers = $Teacher->paginate($pageSize);
 
             // 向V层传数据
             $this->assign('teachers', $teachers);
@@ -25,11 +48,15 @@ class TeacherController extends Controller
 
             // 将数据返回给用户
             return $htmls;
+
+        // 获取到ThinkPHP的内置异常时，直接向上抛出，交给ThinkPHP处理
+        } catch (\think\Exception\HttpResponseException $e) {
+             throw $e;
+
+          // 获取到正常的异常时，输出异常
         } catch (\Exception $e) {
-            // 由于对异常进行了处理，如果发生了错误，我们仍然需要查看具体的异常位置及信息，那么需要将以下代码的注释去掉。
-            // throw $e;
-            return '系统错误' . $e->getMessage();
-        }
+            return $e->getMessage();
+        } 
     }
 
     /**
@@ -101,8 +128,10 @@ class TeacherController extends Controller
      public function delete()
     {
         try {
-            // 获取get数据
+            // 实例化请求类
             $Request = Request::instance();
+
+            // 获取get数据
             $id = Request::instance()->param('id/d');
             
             // 判断是否成功接收
@@ -120,35 +149,36 @@ class TeacherController extends Controller
 
             // 删除对象
             if (!$Teacher->delete()) {
-                $message = '删除失败:' . $Teacher->getError();
+                return $this->error('删除失败:' . $Teacher->getError());
             }
 
-            // 进行跳转
-            return $this->success('删除成功', $Request->header('referer')); 
-        } catch (\Exception $e) {
-            var_dump($e);
+        // 获取到ThinkPHP的内置异常时，直接向上抛出，交给ThinkPHP处理
+        } catch (\think\Exception\HttpResponseException $e) {
+        throw $e;
+
+        // 获取到正常异常时，输出异常
+        } catch (\Exception $e) {          
             return $e->getMessage();
         }
 
-        return $this->error($message);
+        // 进行跳转
+        return $this->success('删除成功', $Request->header('referer'));
     }
 
    
    
     public function test()
     {
-        try {
-            throw new \Exception("Error Processing Request", 1);
-            return $this->error("系统发生错误");
+        $pageSize = 5; // 每次显示5条数据
+        $Teacher = new Teacher;
 
-        // 获取到ThinkPHP的内置异常时，直接向上抛出，交给ThinkPHP处理
-        } catch (\think\Exception\HttpResponseException $e) {
-            throw $e;
+        // 调用分页
+        $teachers = $Teacher->paginate($pageSize);
+        var_dump($teachers->count()); // 调用count（）方法
 
-        // 获取到正常的异常时，输出异常
-        } catch (\Exception $e) {
-            return $e->getMessage();
-        } 
+        // 不调用分页
+        $teachers = $Teacher->select();
+        var_dump($teachers);
     }
 
 
@@ -160,57 +190,76 @@ class TeacherController extends Controller
      */
     public function edit()
     {
-        // 获取传入ID
-        $id = Request::instance()->param('id/d');
+        try
+        {   
+            // 获取传入ID
+            $id = Request::instance()->param('id/d');
 
-        // 在Teacher表模型中获取当前记录
-        if (is_null($Teacher = Teacher::get($id))) {
-            return '系统未找到ID为' . $id . '的记录';
-        } 
-        
-        // 将数据传给V层
-        $this->assign('Teacher', $Teacher);
+            // 判断是否成功接收
+            if (is_null($id) || 0 === $id) {
+                throw new \Exception('未获取到ID信息', 1);
+            }
+    
+            // 在Teacher表模型中获取当前记录
+            if (null === $Teacher = Teacher::get($id)) 
+            {
+                // 由于在$this->error抛出了异常，所以也可以省略return（不推荐）
+                $this->error ('系统未找到ID为' . $id . '的记录');
+            } 
+            
+            // 将数据传给V层
+            $this->assign('Teacher', $Teacher);
+    
+            // 获取封装好的V层内容
+            $htmls = $this->fetch();
+    
+            // 将封装好的V层内容返回给用户
+            return $htmls;
 
-        // 获取封装好的V层内容
-        $htmls = $this->fetch();
+        // 获取到ThinkPHP的内置异常时，直接向上抛出，交给ThinkPHP处理
+        } catch (\think\Exception\HttpResponseException $e) {
+            throw $e;
 
-        // 将封装好的V层内容返回给用户
-        return $htmls;
+        // 获取到正常异常时，输出异常
+        } catch (\Exception $e) {
+            return $e->getMessage();
+        }
     }
     public function update()
     {
         try {
         // 接收数据，获取要更新的关键字信息
         $id = Request::instance()->post('id/d');
-        $message = '更新成功';
 
         // 获取当前对象
         $Teacher = Teacher::get($id);
 
         if(!is_null($Teacher)) {
         // 写入要更新的数据
-        $Teacher->name = Request::instance()->post('name');
-        $Teacher->username = Request::instance()->post('username');
-        $Teacher->sex = Request::instance()->post('sex');
-        $Teacher->email = Request::instance()->post('email');
+        $Teacher->name = input('post.name');
+        $Teacher->username = input('post.username');
+        $Teacher->sex = input('post.sex');
+        $Teacher->email = input('post.email');
 
         // 更新
         if(false === $Teacher->validate(true)->save()) 
         {
-            $message = '更新失败' . $Teacher->getError();
+            return $this->error('更新失败' . $Teacher->getError());
         }
+        } else  {
+            throw new \Exception("所更新的记录不存在",1);  // 调用PHP内置类时，需要在前面加上\
+        }
+    // 获取到ThinkPHP的内置异常时，直接向上抛出，交给ThinkPHP处理    
+    } catch (\think\Exception\HttpResponseException $e) {
+         throw $e;
 
-    } else  {
-        throw new \Exception("所更新的记录不存在",1);  // 调用PHP内置类时，需要在前面加上\
-    }
-} catch (\Exception $e) {
-        // 由于对异常进行了处理，如果发生了错误，我们仍然需要查看具体的异常位置及信息，那么需要将以下的代码的注释去掉
+        // 获取到正常的异常时，输出异常
+        } catch (\Exception $e) {
+            return $e->getMessage();
+        } 
         
-        // throw $e;
-        $message = $e->getMessage();
-    }
-
-    return $message;
+        // 成功跳转至index触发器
+        return $this->success('操作成功', url('index'));
     }
 }
 
